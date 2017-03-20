9b3f0b92aaf8e96af94b6513d4e646e1c5d1d619
iPapatsoris <giannhs95@yahoo.gr>
2017-03-20 05:38:40 +0200

    Switch to SableCC: initial lexical analysis commit

---
+++ src/main/java/compiler/Main.java
@@ -0,0 +1,25 @@
+package compiler;
+
+import compiler.lexer.Lexer;
+import compiler.node.*;
+import java.io.PushbackReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String args[]) {
+        PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
+        Lexer lexer = new Lexer(reader);
+        for(;;) {
+            try {
+            	Token t = lexer.next();
+        		if (t instanceof EOF)
+                	break;
+            	System.out.println(t.getClass() + ": " +  t.toString());
+        	} catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
+        System.exit(0);
+    }
+}
