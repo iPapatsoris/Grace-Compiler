8c11b8f54003e08eee582ec4c427b5492b1129db
iPapatsoris <giannhs95@yahoo.gr>
2017-05-01 08:56:45 +0300

    Initial AST commit

--- src/main/java/compiler/PrintTree.java
+++ src/main/java/compiler/PrintTree.java
@@ -3,12 +3,121 @@ package compiler;
 import compiler.analysis.DepthFirstAdapter;
 import compiler.node.*;
 
+import java.util.Collections;
+
+
 public class PrintTree extends DepthFirstAdapter {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
+
+    private int indentation = 0;
+    private void addIndentationLevel() {
+        indentation++;
+    }
+
+    private void removeIndentationLevel() {
+        indentation--;
+    }
+
+    private void printIndentation() {
+        System.out.print(String.join("", Collections.nCopies(indentation, " ")));
+    }
 
     @Override
     public void defaultIn(Node node) {
         System.out.println(ANSI_RED + node.getClass() + ": " + ANSI_RESET + node);
     }
+
+/*    @Override
+    public void defaultOut(Node node) {
+        removeIndentationLevel();
+    }
+*/
+
+/* In */
+
+    @Override
+    public void inAAddExpr(AAddExpr node) {
+        printIndentation();
+        System.out.println("AddExpr");
+        addIndentationLevel();
+    }
+
+    @Override
+    public void inASubExpr(ASubExpr node) {
+        printIndentation();
+        System.out.println("ASubExpr");
+        addIndentationLevel();
+    }
+
+    @Override
+    public void inAMultExpr(AMultExpr node) {
+        printIndentation();
+        System.out.println("AMultExpr");
+        addIndentationLevel();
+    }
+
+    @Override
+    public void inADivExpr(ADivExpr node) {
+        printIndentation();
+        System.out.println("ADivExpr");
+        addIndentationLevel();
+    }
+
+    @Override
+    public void inAModExpr(AModExpr node) {
+        printIndentation();
+        System.out.println("AModExpr");
+        addIndentationLevel();
+    }
+
+    @Override
+    public void inAIntConstantExpr(AIntConstantExpr node) {
+        printIndentation();
+        System.out.println(node);
+    }
+
+    @Override
+    public void inACharConstantExpr(ACharConstantExpr node) {
+        printIndentation();
+        System.out.println(node);
+    }
+
+/* Out */
+
+    @Override
+    public void outAAddExpr(AAddExpr node) {
+        removeIndentationLevel();
+    }
+
+    @Override
+    public void outASubExpr(ASubExpr node) {
+        removeIndentationLevel();
+    }
+
+    @Override
+    public void outAMultExpr(AMultExpr node) {
+        removeIndentationLevel();
+    }
+
+    @Override
+    public void outADivExpr(ADivExpr node) {
+        removeIndentationLevel();
+    }
+
+    @Override
+    public void outAModExpr(AModExpr node) {
+        removeIndentationLevel();
+    }
+
+    @Override
+    public void outAIntConstantExpr(AIntConstantExpr node) {
+
+    }
+
+    @Override
+    public void outACharConstantExpr(ACharConstantExpr node) {
+
+    }
+
 }
--- src/tests/input.grace
+++ src/tests/input.grace
@@ -1,3 +1,1 @@
-fun main () : nothing {
-    myVar <- adfasd12;
-}
+((1+3) * 4+5)
