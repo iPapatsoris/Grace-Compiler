ca0a6a4e48ba32b14c616b1cb63f0eddb3af8e09
iPapatsoris <giannhs95@yahoo.gr>
2017-07-24 21:31:22 +0300

    Cleanup

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -12,7 +12,6 @@ import java.io.InputStreamReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.IllegalArgumentException;
-import java.lang.Runtime;
 
 public class Main {
 
